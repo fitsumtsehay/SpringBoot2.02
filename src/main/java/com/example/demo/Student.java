3db1ba88c7d1506d6049a39751037c190299ad7a
fitsumtsehay <tsehayfitsum@gmail.com>
2020-05-19 04:53:15 -0400

    SpringBoot202

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Student {
+    private String name;
+    private int age;
+    private double height;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
